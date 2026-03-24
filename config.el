dffee9ac42e348e5e33f40b543c366546c69f3d2
Omar Mohamed <omarcoptan9@gmail.com>
2026-03-24 02:52:12 +0200

    feat(default): tweak some default options
    Change-Id: I9361b49e73356e85fd748f5696297113b834aa04

---
+++ config.el
@@ -0,0 +1,7 @@
+(electric-pair-mode 1)
+(setq org-edit-src-content-indentation 0) ;; Set src block automatic indent to 0 instead of 2.
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(global-display-line-numbers-mode)
+(setq display-line-numbers-type 'relative)
