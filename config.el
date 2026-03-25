a72c884fbddac7f1d1db1f5ee99ea8def2b1146b
Omar Mohamed <omarcoptan9@gmail.com>
2026-03-25 15:14:02 +0200

    feat: add dashboard
    Change-Id: Iac955c1070949b2323b32bbf7d0a5293d41cd5f2

--- config.el
+++ config.el
@@ -99,6 +99,21 @@
 (global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
 (global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
 
+(use-package dashboard
+  :straight t
+  :init
+   (setq initial-buffer-choice 'dashboard-open)
+   (setq doom-fallback-buffer-name "*dashboard*")
+   (add-hook 'doom-enter-frame-hook #'dashboard-open)
+   (setq dashboard-startup-banner 'logo)
+   (setq dashboard-banner-logo-title "Greetings, hack!")
+   (setq dashboard-set-heading-icons t)
+   (setq dashboard-footer-icon "☠")
+   (setq dashboard-footer-messages '("Eat, Sleep, Hack, Repeat..."))
+   (setq initial-scratch-message nil)
+  :config
+   (dashboard-setup-startup-hook))
+
 (use-package nord-theme
   :straight t
   :init
