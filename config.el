1e8517c8f73e0765378c3e01e2e03d8c51f5896a
Omar Mohamed <omarcoptan9@gmail.com>
2026-03-26 17:22:31 +0200

    feat: add hl-todo
    Change-Id: Ie57256fc5c989b4705576739b8aba8337ea0d030

--- config.el
+++ config.el
@@ -182,6 +182,20 @@
   :commands toc-org-enable
   :hook (org-mode-hook . toc-org-enable))
 
+(use-package hl-todo
+  :straight t
+  :hook ((org-mode . hl-todo-mode)
+         (prog-mode . hl-todo-mode))
+  :config
+  (setq hl-todo-highlight-punctuation ":"
+        hl-todo-keyword-faces
+        `(("TODO"       warning bold)
+          ("FIXME"      error bold)
+          ("HACK"       font-lock-constant-face bold)
+          ("REVIEW"     font-lock-keyword-face bold)
+          ("NOTE"       success bold)
+          ("DEPRECATED" font-lock-doc-face bold))))
+
 (use-package ivy
   :straight t
   :bind
