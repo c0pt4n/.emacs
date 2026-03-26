ba127f7e97b8831371a255ab7fd7212b46654dd5
Omar Mohamed <omarcoptan9@gmail.com>
2026-03-26 16:48:24 +0200

    feat: add which-key
    Change-Id: I5616627205c7adc4f10c9ea6f715ef858a5a7c03

--- config.el
+++ config.el
@@ -236,3 +236,20 @@
 (setq display-line-numbers-type 'relative)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
+
+(use-package which-key
+  :init
+  (which-key-mode 1)
+  (setq which-key-side-window-location 'bottom
+	  which-key-sort-order #'which-key-key-order-alpha
+	  which-key-allow-imprecise-window-fit nil
+	  which-key-sort-uppercase-first nil
+	  which-key-add-column-padding 1
+	  which-key-max-display-columns nil
+	  which-key-min-display-lines 6
+	  which-key-side-window-slot -10
+	  which-key-side-window-max-height 0.25
+	  which-key-idle-delay 0.8
+	  which-key-max-description-length 25
+	  which-key-allow-imprecise-window-fit nil
+	  which-key-separator " → " ))
