9cc7a0b2d87524724fc98b1d6de69368f4b4ebaf
Omar Mohamed <omarcoptan9@gmail.com>
2026-04-03 21:18:43 +0200

    config: add general keybindings
    Change-Id: Ia62f9ebee84fd033554628f8de9623f043272dba

--- config.el
+++ config.el
@@ -92,6 +92,56 @@
   :config
   (evil-commentary-mode 1))
 
+(use-package general
+  :demand t
+  :config
+  (general-evil-setup)
+
+  ;; set up 'SPC' as the global leader key
+  (general-create-definer me/leader-keys
+    :states '(normal insert visual emacs)
+    :keymaps 'override
+    :prefix "SPC" ;; set leader
+    :global-prefix "M-SPC") ;; access leader in insert mode
+
+  (me/leader-keys
+    "." '(find-file :wk "Find file"))
+
+  (me/leader-keys
+    "b" '(:ignore t :wk "Buffers")
+    "b i" '(ibuffer :wk "Ibuffer")
+    "b b" '(switch-to-buffer :wk "Switch buffer")
+    "b k" '(kill-current-buffer :wk "Kill this buffer")
+    "b K" '(kill-some-buffer :wk "Kill multiple buffer")
+    "b n" '(next-buffer :wk "Next buffer")
+    "b p" '(previous-buffer :wk "Previous buffer")
+    "b r" '(revert-buffer :wk "Reload buffer"))
+
+  (me/leader-keys
+    "d" '(:ignore t :wk "Dired")
+    "d d" '(dired :wk "Open dired"))
+
+  (me/leader-keys
+    "f" '(:ignore t :wk "Files")
+    "f c" '((lambda() (interactive)
+	      (find-file (expand-file-name "config.org" user-emacs-directory)))
+	    :wk "Open emacs config.org")
+    "f e" '((lambda() (interactive)
+	      (dired user-emacs-directory))
+	    :wk "Open user-emacs-directory in dired"))
+
+  (me/leader-keys
+    "w" '(:ignore t :wk "Window")
+    "w s" '(evil-window-split :wk "Horizontal split")
+    "w v" '(evil-window-vsplit :wk "Vertical split")
+    "w h" '(evil-window-left :wk "Window left")
+    "w l" '(evil-window-right :wk "Window right")
+    "w k" '(evil-window-up :wk "Window up")
+    "w j" '(evil-window-down :wk "Window down")
+    "w c" '(evil-window-delete :wk "Close window")
+    "w n" '(evil-window-new :wk "New window"))
+  )
+
 (use-package nord-theme
   :demand t
   :init
