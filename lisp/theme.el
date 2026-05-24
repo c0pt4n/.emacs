de67933358e886c5d7b8f8805a109c8607c7407a
Omar Mohamed <omarcoptan9@gmail.com>
2026-05-24 18:03:45 +0300

    feat(themes): switch to catppuccin
    Change-Id: Ie460cb0f826ff205e1fbe701a9e6ab3aff68fc22

--- lisp/theme.el
+++ lisp/theme.el
@@ -1,8 +1,11 @@
 ;;; theme.el --- Nord theme -*- lexical-binding: t; -*-
 
-(use-package nord-theme
-  :demand t
+(use-package catppuccin-theme
+  :ensure (catppuccin-theme :host github :repo "catppuccin/emacs" :files ("*.el"))
   :init
-  (load-theme 'nord t))
+  (load-theme 'catppuccin :no-confirm)
+  :config
+  (setq catppuccin-flavor 'frappe) ;; or 'latte, 'macchiato, or 'mocha
+  (catppuccin-reload))
 
 (provide 'theme)
