e88cb5dc882f42f38188d8b62519a0a0b98557fa
Omar Mohamed <omarcoptan9@gmail.com>
2026-06-06 17:54:09 +0300

    feat(vterm): disable modeline and confirm-kill-process
    Change-Id: I42b573333f897616cc88e98d4fbe9e0edda2731e

--- lisp/vterm-config.el
+++ lisp/vterm-config.el
@@ -1,6 +1,10 @@
 ;;; vterm-config.el --- Description -*- lexical-binding: t; -*-
 
 (use-package vterm
+  :hook
+  (vterm-mode . (lambda ()
+                  (setq-local confirm-kill-processes nil
+                              mode-line-format nil)))
   :init
   (setq vterm-timer-delay 0.05
 		vterm-max-scrollback 5000)
